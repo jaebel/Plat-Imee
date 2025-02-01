96e6ee9b0e17d7fa36295ec823bee47bc7ca170a
Jaden Bellevue <j.p.bellevue@se21.qmul.ac.uk>
2025-02-01 01:23:03 +0000

    Add spring initializr template

---
+++ src/main/kotlin/com/platimee/spring_platimee/SpringPlatimeeApplication.kt
@@ -0,0 +1,11 @@
+package com.platimee.spring_platimee
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringPlatimeeApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringPlatimeeApplication>(*args)
+}
---
+++ src/test/kotlin/com/platimee/spring_platimee/SpringPlatimeeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.platimee.spring_platimee
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringPlatimeeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
