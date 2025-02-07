72c4bea521ce2f00f5aeb1544d3d021b915d9fb7
Jaden Bellevue <j.p.bellevue@se21.qmul.ac.uk>
2025-02-07 21:42:51 +0000

    Add get user endpoint

---
+++ src/main/kotlin/com/platimee/spring_platimee/entrypoint/GetUserController.kt
@@ -0,0 +1,32 @@
+package com.platimee.spring_platimee.entrypoint
+
+import com.platimee.spring_platimee.model.User
+import com.platimee.spring_platimee.service.GetUserService
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+class GetUserController(private val getUserService: GetUserService) {
+
+    // GET all users
+    @GetMapping(
+        path = ["/api/v1/users"],
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    fun getAllUsers(): ResponseEntity<List<User>> {
+        val users = getUserService.getAll()
+        return ResponseEntity.status(HttpStatus.OK).body(users)
+    }
+
+    // GET a single user by ID
+    @GetMapping(
+        path = ["/api/v1/users/{userId}"],
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    fun getUserById(@PathVariable userId: Long): ResponseEntity<User> {
+        val user = getUserService.getById(userId)
+        return ResponseEntity.status(HttpStatus.OK).body(user)
+    }
+}
