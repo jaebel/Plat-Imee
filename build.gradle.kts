92f637b30ead6fb0fe2880ad9350503f744ae913
Jaden Bellevue <j.p.bellevue@se21.qmul.ac.uk>
2025-02-09 01:04:00 +0000

    Get kotest working

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,10 +43,16 @@ dependencies {
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("org.postgresql:postgresql")
 	annotationProcessor("org.projectlombok:lombok")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testImplementation("org.springframework.security:spring-security-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	testImplementation("io.kotest:kotest-runner-junit5:5.8.0")
+	testImplementation("io.kotest:kotest-assertions-core:5.8.0")
+	testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+	testImplementation("io.mockk:mockk:1.13.10")
 }
 
 kotlin {
---
+++ src/test/kotlin/com/platimee/spring_platimee/entrypointTests/CreateUserControllerTests.kt
@@ -0,0 +1,10 @@
+package com.platimee.spring_platimee.entrypointTests
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class CreateUserControllerTests: StringSpec({
+    "1 + 1 should equal 2" {
+        (1 + 1) shouldBe 2
+    }
+})
