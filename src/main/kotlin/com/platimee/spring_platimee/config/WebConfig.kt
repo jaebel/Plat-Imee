ef3d8977fc52a1cc04af65dd7632ff8625ea0cb2
Jaden Bellevue <j.p.bellevue@se21.qmul.ac.uk>
2025-02-27 18:35:29 +0000

    Add web config to get past CORS error

---
+++ src/main/kotlin/com/platimee/spring_platimee/config/WebConfig.kt
@@ -0,0 +1,19 @@
+package com.platimee.spring_platimee.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+@EnableWebMvc
+class WebConfig : WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/api/**")
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+    }
+}
