09ae4a398df949708c96cd61ba5216363c6f6bdb
Jaden Bellevue <j.p.bellevue@se21.qmul.ac.uk>
2025-03-02 22:09:59 +0000

    quick fix

--- src/main/kotlin/com/platimee/spring_platimee/anime/repository/GenreRepository.kt
+++ src/main/kotlin/com/platimee/spring_platimee/anime/repository/GenreRepository.kt
@@ -3,4 +3,6 @@ package com.platimee.spring_platimee.anime.repository
 import com.platimee.spring_platimee.anime.model.Genre
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface GenreRepository : JpaRepository<Genre, Long>
+interface GenreRepository : JpaRepository<Genre, Long>{
+    fun findByName(genreName: String): Genre?
+}
